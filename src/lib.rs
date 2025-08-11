86ca2ae1c66d2d3b3da33a7216c2fd41db002136
saki-kaguya <222018923+saki-kaguya@users.noreply.github.com>
2025-08-11 21:46:29 +0800

    Updated lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ pub use parser::{
     generate as generate_unit_order_from_units, load_and_generate as generate_unit_order_from_path,
     load_unit as load_unit_from_path,
 };
+pub use unit::UnitType;
